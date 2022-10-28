dd0aad0fe410fe40fdf9140431f1702016e3ee6d
Bhathiya Gamage <73831309+horapusa-lk@users.noreply.github.com>
2022-10-28 18:19:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,22 @@
 cd ~
+# Update package
 apt update
 apt install git
+
+#Install python
 apt install python3
 apt install python3-pip
+#Install my script
 git clone https://github.com/horapusa-lk/hp-v2
 cd hp-v2
 chmod +x *
 pip3 install -r requirements.txt
 
-echo Enter a bot token :
-read bot_token
-echo BOT_TOKEN=""$bot_token"" > /usr/bin/configs.py
-
-echo Enter your telegram user id :
-read SUDO_ID
-echo SUDO_ID=$SUDO_ID >> /root/configs.py
+python3 env_vars.py
 
 cp /root/configs.py /usr/bin/
 cp bot.py /usr/bin/
 cp bot.service /lib/systemd/system/
 sudo systemctl enable bot.service
 bash v2ray.sh
-
-
+reboot
