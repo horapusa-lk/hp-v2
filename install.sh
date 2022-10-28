13dedbe302425abff6c251da8c2e5db9aeaa57cf
Bhathiya Gamage <73831309+horapusa-lk@users.noreply.github.com>
2022-10-28 16:42:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,15 @@ git clone https://github.com/horapusa-lk/hp-v2
 cd hp-v2
 chmod +x *
 pip3 install -r requirements.txt
+
+echo Enter a bot token :
+read bot_token
+echo BOT_TOKEN=$bot_token > /root/configs.py
+
 cp configs.py /usr/bin/
 cp bot.py /usr/bin/
 cp bot.service /lib/systemd/system/
 sudo systemctl enable test-py.service
 bash v2ray.sh
 
-echo Enter a bot token :
-read bot_token
-echo BOT_TOKEN=$bot_token > /root/configs.py
+
